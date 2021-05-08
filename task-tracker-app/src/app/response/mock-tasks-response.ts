72f6c4fec5b5984d7aed5177c299ff0ae7bf8d67
Akshay G <akshayg14@gmail.com>
2021-05-08 19:54:43 +0530

    Added a mock task response

---
+++ task-tracker-app/src/app/response/mock-tasks-response.ts
@@ -0,0 +1,9 @@
+import { Task } from '../models/Task';
+
+export const tasks: Task[] = [
+  { id: 1, text: 'Task 1', day: 'May 8th at 12:00pm', reminder: true },
+  { id: 2, text: 'Task 2', day: 'May 8th at 07:30pm', reminder: false },
+  { id: 3, text: 'Task 3', day: 'May 9th at 12:45pm', reminder: true },
+  { id: 4, text: 'Task 4', day: 'May 10th at 08:00am', reminder: false },
+  { id: 5, text: 'Task 5', day: 'May 10th at 10:30am', reminder: false }
+];
