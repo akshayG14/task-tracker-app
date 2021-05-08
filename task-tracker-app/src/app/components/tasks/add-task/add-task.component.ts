1359f0ae4b89506fd4bb40941549ed71285192db
Akshay G <akshayg14@gmail.com>
2021-05-08 21:56:08 +0530

    Added add-task component

---
+++ task-tracker-app/src/app/components/tasks/add-task/add-task.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.scss']
+})
+export class AddTaskComponent implements OnInit {
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void { }
+
+}
